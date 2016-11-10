d06b4ddda04d3f527f8628d758b2b064bb41c00e
Alexander <viertual@gmail.com>
2016-11-10 11:59:20 +0300

    Transform component intro.

--- CPP/Components/Transform/Transform.cpp
+++ CPP/Components/Transform/Transform.cpp
@@ -1,9 +1,9 @@
-//
-//  Transform.cpp
-//  OME
-//
-//  Created by Alexander Kolesnikov on 10/11/2016.
-//  Copyright © 2016 Daniel Ginsburg. All rights reserved.
-//
-
 #include "Transform.hpp"
+
+
+Transform::Transform(GameObject * const gameObject) : IComponent(gameObject){
+
+}
+
+Transform::~Transform(){}
+
--- CPP/Components/Transform/Transform.hpp
+++ CPP/Components/Transform/Transform.hpp
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "IComponent.hpp"
+#include "glm.hpp"
+#include "quaternion.hpp"
+
+using vec3 = glm::vec3;
+using mat4 = glm::mat4;
+
+struct Transform : public IComponent{
+
+    Transform(GameObject * const gameObject);
+    virtual ~Transform();
+    
+    vec3 mPosition;
+    vec3 mScale;
+    vec3 mRotation;
+    vec3 mFront;
+    vec3 mUp;
+    vec3 mRight;
+    
+    mat4 mMatrix;
+};
